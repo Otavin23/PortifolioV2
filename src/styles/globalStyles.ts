beff10b28fb227bc0653f5e684683a722645c9fb
Otavin23 <alvesotavio132@gmail.com>
2022-09-05 21:31:21 -0300

    scrool-bard

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -5,11 +5,20 @@ export const CreateGlobalStyles = createGlobalStyle`
         padding: 0;
         margin: 0; 
         box-sizing: border-box;
-        font-family: 'Roboto', sans-serif;
     }
     :root{
         --background: #161616;
         --backgroundTwo: #0B0B0B;
         --backgroundMark:#034A73;
-  }
+    }
+    ::-webkit-scrollbar {
+        width:10px;
+        height: 10px;
+    }
+    ::-webkit-scrollbar-track {
+        background:#333;
+    }
+    ::-webkit-scrollbar-thumb {
+        background: #2e9dd8;
+    }
 `;
